7dc9569b7b103f604fbb6297b405b6cf41632909
Leonardo Omaña <leotangram@gmail.com>
2020-06-23 16:06:58 -0500

    Const let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,20 @@
+// Variables and Constants
+
+const name = 'Leo'
+const surname = 'Omaña'
+
+let diceValue = 5
+diceValue = 4
+
+console.log(name, surname, diceValue)
+
+// var no se debe usar
+
+if (true) {
+  let diceValue = 6
+  const name = 'Peter'
+  console.log(diceValue)
+  console.log(name)
+}
+
+console.log(diceValue)
