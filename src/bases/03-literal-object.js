e299490ae3e0fd3786e9bf054037bbbfe003972d
Leonardo Omaña <leotangram@gmail.com>
2020-06-23 16:23:53 -0500

    Literal object

---
+++ src/bases/03-literal-object.js
@@ -0,0 +1,17 @@
+const person = {
+  name: 'Tony',
+  surname: 'Stark',
+  age: 45,
+  address: {
+    city: 'New York',
+    zip: 13423423,
+    lat: 14.32443,
+    lng: 34.93434
+  }
+}
+
+const person2 = { ...person }
+person2.name = 'Peter'
+console.log(person)
+
+console.log(person2)
