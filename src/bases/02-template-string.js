4192c77f63b5e1bb2c35e0f5dca1d328de99cb63
Leonardo Omaña <leotangram@gmail.com>
2020-06-23 16:13:39 -0500

    Template string

---
+++ src/bases/02-template-string.js
@@ -0,0 +1,14 @@
+const name = 'Leo'
+const surname = 'Omaña'
+
+const completeName = `
+${name} 
+${surname}
+${1 + 1}
+`
+console.log(completeName)
+function getSaludo(name) {
+  return `Hola ${name}`
+}
+
+console.log(`Este es un texto: ${getSaludo(name)}`)
