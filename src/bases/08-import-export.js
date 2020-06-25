cecf6d005938d9bdbf2b19d30ce88ac77ce8815d
Leonardo Omaña <leotangram@gmail.com>
2020-06-25 16:55:36 -0500

    Import export

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,10 @@
+// Imports y exports
+import heroes, { owners } from '../data/heroes'
+
+console.log(owners)
+
+const getHeroeById = id => heroes.find(heroe => heroe.id === id)
+console.log(getHeroeById(2))
+
+const getHeroesByOwner = owner => heroes.filter(heroe => heroe.owner === owner)
+console.log(getHeroesByOwner('DC'))
